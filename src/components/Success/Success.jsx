4220c0720a909f676c84eba44062332bb8066f62
Javier Veron <javier.veron@gmail.com>
2022-09-30 19:24:53 -0300

    Se realizo un Fix en el mensaje del Componente Success

--- src/components/Success/Success.jsx
+++ src/components/Success/Success.jsx
@@ -7,7 +7,7 @@ const Success = ({id}) => {
             <div className="col-md-12 text-center p-5">
                 <h1>Felicitaciones!</h1>
                 <p><img src={Logo} alt="BurgerKing" width="180" /></p>
-                <p>La Orden de Compra de Compra se generó con el ID: <b>{id}</b></p>
+                <p>La Orden de Compra se generó con el ID: <b>{id}</b></p>
             </div>
         </div>
     )
